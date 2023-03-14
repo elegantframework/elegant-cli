b69b7d62c2fe3f36366c390bf3dc957177988d6e
Brandon Owens <brandono@vitamins2you.com>
2023-03-13 20:02:11 -0400

    Ready for npm publish early alpha testing.
    Cleaned up the last rough bits ahead of npm publish and early alpha testing. This is just an MVP and needs a ton of features still.

--- source/init.js
+++ source/init.js
@@ -1,16 +1,20 @@
 #! /usr/bin/env node
+const fs =  require('fs');
 const chalk = require('chalk');
 const clear = require('clear');
 const figlet = require('figlet');
 const path = require('path');
 const program = require('commander');
+const arg = require('arg');
 
 clear();
-console.log(
-  chalk.green.bold(
-    figlet.textSync('Elegant CLI :)', { horizontalLayout: 'full' })
-  )
-);
+
+fs.cp('./source/elegant-docs/', './', { recursive: true }, (err) => {
+  if (err) {
+    console.error(err);
+  }
+});
+
 console.log(
   chalk.yellow.bgBlack(
     figlet.textSync("Let's create some docs!", { horizontalLayout: 'full' })
