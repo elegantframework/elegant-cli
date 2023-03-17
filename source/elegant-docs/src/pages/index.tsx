a6562d3ef36b1c118126d0e5dc43c937e6533354
Brandon Owens <brandono@vitamins2you.com>
2023-03-16 23:10:11 -0400

    Update index.tsx

--- source/elegant-docs/src/pages/index.tsx
+++ source/elegant-docs/src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Layout from "@theme/Layout";
+import React from "react";
+
 export default function Home(): JSX.Element {
   //
   // Temporarily redirect to the docs until we find a workaround for Docusaurus' '/' slug handling.
   //
-  window.location.href = '/docs';
-  return null;
+  if (typeof window !== "undefined") {
+    window.location.href = '/docs';
+    return null;
+  }
+  else{
+    return(<Layout>{null}</Layout>);
+  }
 }
