3b4617746eeb712f91707c2f5aba87846e991caa
Brandon Owens <brandono@vitamins2you.com>
2023-05-17 22:46:01 -0400

    Revert "2.2.2-prerelease.0"
    This reverts commit 3430c244b73ae1f9cb4685e795ce526a0d0642f5.

--- scripts/publish-innovators-prerelease.ts
+++ scripts/publish-innovators-prerelease.ts
@@ -1,7 +1,7 @@
 const spawn = require('cross-spawn');
 
 spawn.sync(
-    "npm version prerelease --preid=prerelease && npm publish --tag prerelease",
+    "npm version prerelease --preid=innovators && npm publish --tag innovators",
     {
         stdio: 'inherit'
     }
