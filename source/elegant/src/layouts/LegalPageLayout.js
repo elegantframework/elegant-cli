c3f11046128cb7e0f097ef3541724521c9434471
Brandon Owens <brandono@vitamins2you.com>
2023-06-01 09:54:48 -0400

    Update LegalPageLayout.js

--- source/elegant/src/layouts/LegalPageLayout.js
+++ source/elegant/src/layouts/LegalPageLayout.js
@@ -1,6 +1,6 @@
 export function LegalPageLayout({ children }) {
   return (
-    <main className="max-w-3xl mx-auto relative z-20 pt-10 pb-20 sm:pb-10 px-6 sm:px-8 md:px-10 xl:max-w-none">
+    <main className="max-w-3xl mx-auto relative z-20 pt-10 pb-20 sm:pb-10 px-6 sm:px-8 md:px-10">
       {children}
     </main>
   )
