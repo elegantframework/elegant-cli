2431e100033a0a911dd769fe500146a2ba5f9eba
Brandon Owens <brandono@vitamins2you.com>
2023-06-20 16:48:45 -0400

    Added the core shell of the admin panel
    It builds, but all tests are not passing

---
+++ source/elegant/src/utils/core/Apollo/apollo.ts
@@ -0,0 +1,85 @@
+import { Session } from '@/types/Index';
+import {
+  ApolloClient,
+  from,
+  createHttpLink,
+  InMemoryCache,
+  NormalizedCacheObject
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import fetch from 'cross-fetch';
+import { useMemo } from 'react';
+
+let apolloClient: ApolloClient<NormalizedCacheObject | null>
+
+const apolloCache = new InMemoryCache({
+  typePolicies: {}
+})
+
+async function getSession() {
+  const response = await fetch('/api/admin/user')
+  return response.json()
+}
+
+function createApolloClient(session?: Session | null) {
+  const httpLink = createHttpLink({
+    uri: 'https://api.github.com/graphql',
+    // Prefer explicit `window.fetch` when available so that outgoing requests
+    // are captured and deferred until the Service Worker is ready. If no window
+    // or window.fetch, default to cross-fetch's ponyfill
+    fetch: (...args) =>
+      (typeof window !== 'undefined' && typeof window.fetch === 'function'
+        ? window.fetch
+        : fetch)(...args)
+  })
+
+  const authLink = setContext(async (_, { headers }: { headers: Headers }) => {
+    const data: { session: Session } = session
+      ? { session }
+      : await getSession()
+    const modifiedHeader = {
+      headers: {
+        ...headers,
+        authorization: data.session?.access_token
+          ? `Bearer ${data.session.access_token}`
+          : ''
+      }
+    }
+    return modifiedHeader
+  })
+
+  return new ApolloClient({
+    ssrMode: typeof window === 'undefined',
+    link: from([authLink, httpLink]),
+    cache: apolloCache
+  })
+}
+
+export function initializeApollo(
+  initialState = null,
+  session?: Session | null
+) {
+  // serve para verificar se já existe uma instância, para não criar outra
+  const apolloClientGlobal = apolloClient ?? createApolloClient(session)
+
+  // se a página usar o apolloClient no lado client
+  // hidratamos o estado inicial aqui
+  if (initialState) {
+    apolloClientGlobal.cache.restore(initialState)
+  }
+
+  // sempre inicializando no SSR com cache limpo
+  if (typeof window === 'undefined') return apolloClientGlobal
+  // cria o apolloClient se estiver no client side
+  apolloClient = apolloClient ?? apolloClientGlobal
+
+  return apolloClient
+}
+
+export function useApollo(initialState = null, session?: Session) {
+  const store = useMemo(
+    () => initializeApollo(initialState, session),
+    [initialState, session]
+  )
+  return store
+}
---
+++ source/elegant/src/utils/core/mergeMdMeta.ts
@@ -0,0 +1,68 @@
+import showdown from 'showdown';
+import DOMPurify from 'dompurify';
+import { Document } from '@/types/Index';
+import { replaceImageSrcRoot } from './replaceImageSrc';
+import { IMAGES_PATH } from './constants';
+
+export const mergeMdMeta = (data: Document): string => {
+  const meta = Object.entries(
+    (({ content, publishedAt, ...meta }) => meta)(data)
+  )
+
+  if (data.publishedAt) {
+    meta.push(['publishedAt', data.publishedAt.toISOString()])
+  }
+
+  let merged = '---\n'
+
+  Object.entries(meta).forEach(([_, value]) => {
+    if (Array.isArray(value[1])) {
+      merged += `${value[0]}: ${JSON.stringify(value[1])}\n`
+    } else if (value[1] instanceof Object) {
+      merged += `${value[0]}:\n`
+      Object.entries(value[1]).forEach(([key, value]) => {
+        merged += `  ${key}: '${DOMPurify.sanitize(value as string).replaceAll(
+          "'",
+          "''"
+        )}'\n`
+      })
+    } else {
+      merged += `${value[0]}: '${DOMPurify.sanitize(value[1]).replaceAll(
+        "'",
+        "''"
+      )}'\n`
+    }
+  })
+
+  merged += '---\n\n'
+
+  const converter = new showdown.Converter()
+
+  // replace /api/outstatic/images/ references
+  let newContent = replaceImageSrcRoot(
+    data.content,
+    '/api/outstatic/images/',
+    `/${IMAGES_PATH}`
+  )
+
+  const imgFolderRegex = new RegExp(/(^\/api\/outstatic\/images\/)/gi)
+  newContent = replaceImageSrcRoot(
+    newContent,
+    imgFolderRegex,
+    `/${IMAGES_PATH}`
+  )
+
+  // remove weird <p> tags
+  newContent.replaceAll('<p><br></p>', '').replaceAll('<br></p>', '</p>')
+
+  const markdown = converter.makeMarkdown(newContent)
+
+  // replace leftover html comment with empty line
+  const cleanMarkdown = markdown
+    .replaceAll('\n\n\n<!-- -->\n\n', '\n\n')
+    .replaceAll('](<', '](')
+    .replaceAll('>)', ')')
+
+  merged += cleanMarkdown
+  return merged
+}
