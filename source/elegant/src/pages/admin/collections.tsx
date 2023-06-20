c0e7764f453b477ede2abc6f8f044ce932592aa9
Brandon Owens <brandono@vitamins2you.com>
2023-06-20 18:40:20 -0400

    Update collections.tsx

--- source/elegant/src/pages/admin/collections.tsx
+++ source/elegant/src/pages/admin/collections.tsx
@@ -97,7 +97,7 @@ export default function Collections() {
         <>
           <div className="mb-8 flex h-12 items-center">
             <h1 className="mr-12 text-2xl">Collections</h1>
-            <Link href="/outstatic/collections/new">
+            <Link href="/admin/collections/new">
               <div className="cursor-pointer rounded-lg border px-5 py-2.5 text-sm font-medium focus:outline-none focus:ring-4 border-gray-600 bg-gray-800 text-white hover:border-gray-600 hover:bg-gray-700 focus:ring-gray-700 no-underline">
                 New Collection
               </div>
@@ -109,7 +109,7 @@ export default function Collections() {
                 key={collection}
                 className="relative flex p-6 justify-between items-center max-w-sm bg-white rounded-lg border border-gray-200 shadow-md hover:bg-slate-100"
               >
-                <Link href={`/outstatic/collections/${collection}`}>
+                <Link href={`/admin/collections/${collection}`}>
                   <h5 className="text-2xl cursor-pointer font-bold tracking-tight text-gray-900 capitalize hover:text-blue-500">
                     {collection}
                     <span className="absolute top-0 bottom-0 left-0 right-16"></span>
