2142bea2f7e9e7020cd9cd6f5d3f54afeaaf2422
Shiv Kumar Saxena <74370135+Shiv-2712@users.noreply.github.com>
2023-07-01 19:55:00 +0530

    documentation problem fix

--- source/elegant/src/layouts/DocumentationLayout.js
+++ source/elegant/src/layouts/DocumentationLayout.js
@@ -1,6 +1,6 @@
 import { SidebarLayout } from '@/layouts/SidebarLayout';
 import { Title } from '@/components/Meta';
-import { documentationNav } from '@/navs/documentation';
+import { documentationNav } from '@/config/navigation';
 import Seo from "@/components/core/Seo/Seo";
 
 export function DocumentationLayout(props) {
