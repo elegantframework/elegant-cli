d26da5c14197beb921029aa9d69d3f04b33e276d
Brandon Owens <brandono@vitamins2you.com>
2023-07-06 08:01:52 -0400

    Fixes

--- source/elegant/src/layouts/DocumentationLayout.js
+++ source/elegant/src/layouts/DocumentationLayout.js
@@ -1,7 +1,5 @@
 import { SidebarLayout } from '@/layouts/SidebarLayout';
-import { Title } from '@/components/Meta';
 import { documentationNav } from '@/config/navigation';
-import Seo from "@/components/core/Seo/Seo";
 
 export function DocumentationLayout(props) {
   return (
