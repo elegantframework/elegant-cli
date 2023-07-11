ed193df6e9924ab49500efdbb027bd6131f79c01
Brandon Owens <brandono@vitamins2you.com>
2023-07-11 15:52:39 -0400

    Refactor/remove deprecated header (#122)
    * Update index.js
    
    * Remove deprecated items
    
    * Update Header.js

--- source/elegant/src/components/Header.js
+++ source/elegant/src/components/Header.js
@@ -1,7 +1,5 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import VersionSwitcher from '@/components/core/Headers/VersionSwitcher';
-import { SearchButton } from '@/components/Search';
 import Router from 'next/router';
 import Logo from '@/components/core/Logos/Logo/Logo';
 import { Dialog } from '@headlessui/react';
@@ -11,7 +9,7 @@ import ThemeToggle, {ThemeSelect} from '@/components/core/Toggles/ThemeToggle/Th
 import Config from "Config";
 
 /**
- * @deprecated - To be removed by 2.3
+ * @deprecated - To be removed by version 2.4
  */
 export function NavPopover({ display = 'md:hidden', className, ...props }) {
   let [isOpen, setIsOpen] = useState(false)
@@ -91,7 +89,7 @@ export function NavPopover({ display = 'md:hidden', className, ...props }) {
 }
 
 /**
- * @deprecated - To be removed by 2.3
+ * @deprecated - To be removed by version 2.4
  */
 export function NavItems() {
   const router = useRouter();
@@ -127,6 +125,9 @@ export function NavItems() {
   )
 }
 
+/**
+ * @deprecated - To be removed by version 2.4
+ */
 export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section }) {
   let [isOpaque, setIsOpaque] = useState(false)
 
@@ -194,14 +195,12 @@ export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section 
                   className="mr-3 flex-none w-[2.0625rem] overflow-hidden md:w-auto"
                   onContextMenu={(e) => {
                     e.preventDefault()
-                    // Router.push('/brand')
                   }}
                 >
                   <span className="sr-only">{Config('app.name')} home page</span>
                   <Logo className="w-auto h-7" />
                 </a>
               </Link>
-              {/* <VersionSwitcher /> */}
               <div className="relative hidden lg:flex items-center ml-auto">
                 <nav className="text-sm leading-6 font-semibold text-slate-700 dark:text-slate-200">
                   <ul className="flex space-x-8">
@@ -227,22 +226,6 @@ export function Header({ hasNav = false, navIsOpen, onNavToggle, title, section 
                   </a>
                 </div>
               </div>
-              {/* <SearchButton className="ml-auto text-slate-500 w-8 h-8 -my-1 flex items-center justify-center hover:text-slate-600 lg:hidden dark:text-slate-400 dark:hover:text-slate-300">
-                <span className="sr-only">Search</span>
-                <svg
-                  width="24"
-                  height="24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  aria-hidden="true"
-                >
-                  <path d="m19 19-3.5-3.5" />
-                  <circle cx="11" cy="11" r="6" />
-                </svg>
-              </SearchButton> */}
               <NavPopover className="ml-auto -my-1" display="lg:hidden" />
             </div>
           </div>
