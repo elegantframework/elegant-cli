13a77c652e1bc5950f355c0a07aef974db56a3de
Brandon Owens <brandono@vitamins2you.com>
2023-08-10 18:17:39 -0400

    Added storybook

---
+++ packages/elegant-ui/src/components/Marks/PoweredByElegant/PoweredByElegant.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import PoweredByElegant from './PoweredByElegant';
+
+const meta: Meta<typeof PoweredByElegant> = {
+  title: 'Core/Marks/Powered by Elegant',
+  component: PoweredByElegant,
+};
+
+export default meta;
+type Story = StoryObj<typeof PoweredByElegant>;
+
+export const Primary: Story = {
+  args: {
+    //👇 The args you need here will depend on your component
+  },
+};
