130007691aa1d69a813f83ed9df30580201012f2
Brandon Owens <brandono@vitamins2you.com>
2023-08-10 20:20:16 -0400

    Add tailwind to storybook

--- packages/elegant-ui/.storybook/main.ts
+++ packages/elegant-ui/.storybook/main.ts
@@ -7,6 +7,16 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
+    {
+      name: '@storybook/addon-styling',
+      options: {
+        // Check out https://github.com/storybookjs/addon-styling/blob/main/docs/api.md
+        // For more details on this addon's options.
+        postCss: {
+          implementation: require.resolve('postcss'),
+        },
+      },
+    },
   ],
   framework: {
     name: "@storybook/react-webpack5",
--- packages/elegant-ui/src/index.ts
+++ packages/elegant-ui/src/index.ts
@@ -1,3 +1,1 @@
-import './styles.css';
-
 export * from "./components";
