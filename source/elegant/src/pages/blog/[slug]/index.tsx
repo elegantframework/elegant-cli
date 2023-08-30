33ba726b51dbaf879709328d0c728f7b6f46fa55
Brandon Owens <brandono@vitamins2you.com>
2023-08-30 16:26:10 -0400

    Feature/dynamic blog route (#159)
    * Migrated the main blog page to use the new _content method
    
    * Created the initial blog post layout component
    
    * Added the dynamic blog route

---
+++ source/elegant/src/pages/blog/[slug]/index.tsx
@@ -0,0 +1,186 @@
+import { NewsletterForm } from "@/components/core/NewsletterForm/NewsletterForm";
+import Error from "@/pages/404";
+import { Post } from "@/types/Post";
+import { getDocumentBySlug } from "@/utils/core/Collections/collection";
+import Config from "@/utils/core/Config/Config";
+import MarkdownToHtml from "@/utils/core/Rehype/MarkdownToHtml";
+import moment from "moment";
+import { GetServerSideProps } from "next";
+import { ArticleJsonLd } from "next-seo";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+type Props = {
+    /**
+     * A blog post to be displayed.
+     */
+    post: Post;
+    /**
+     * HTML string content.
+     */
+    content: string;
+};
+
+export default function Index({
+    post,
+    content
+}: Props) {
+    const router = useRouter();
+
+    if (!router.isFallback && !post?.slug) {
+        return(
+            <Error />
+        );
+    }
+
+    return(
+        <>
+            <ArticleJsonLd 
+                useAppDir={false}
+                url={Config('app.url') + router.pathname}
+                title={post.title}
+                images={[
+                    `${Config('app.url')}/images/${post.coverImage}`
+                ]}
+                datePublished={post.publishedAt || ""}
+                authorName={[{
+                    name: post.author?.name,
+                    url: Config('app.twitter_handle').replace('@', '')
+                }]}
+                description={post.description}
+                isAccessibleForFree={true}
+            />
+            <div className="overflow-hidden">
+                <div className="max-w-8xl mx-auto">
+                    <div className="flex px-4 pt-8 pb-10 lg:px-8">
+                        <Link href="/blog">
+                            <a className="group flex font-semibold text-sm leading-6 text-slate-700 hover:text-slate-900 dark:text-slate-200 dark:hover:text-white">
+                                <svg
+                                    viewBox="0 -9 3 24"
+                                    className="overflow-visible mr-3 text-slate-400 w-auto h-6 group-hover:text-slate-600 dark:group-hover:text-slate-300"
+                                >
+                                    <path
+                                        d="M3 0L0 3L3 6"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        strokeWidth="2"
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                    />
+                                </svg>
+                                Go back
+                            </a>
+                        </Link>
+                    </div>
+                </div>
+                <div className="px-4 sm:px-6 md:px-8">
+                    <div className="max-w-3xl mx-auto pb-28">
+                        <main>
+                            <article className="relative pt-10">
+                                <h1 className="text-2xl font-extrabold tracking-tight text-slate-900 dark:text-slate-200 md:text-3xl">
+                                    {post.title}
+                                </h1>
+                                <div className="text-sm leading-6">
+                                    <dl>
+                                        <dt className="sr-only">Date</dt>
+                                        <dd className="absolute top-0 inset-x-0 text-slate-700 dark:text-slate-400">
+                                            <time dateTime={post.publishedAt}>
+                                                {moment.utc(post.publishedAt).format('dddd, MMMM Do YYYY').toString()}
+                                            </time>
+                                        </dd>
+                                    </dl>
+                                </div>
+                                <div className="mt-6">
+                                    <ul className="flex flex-wrap text-sm leading-6 -mt-6 -mx-5">
+                                        <li
+                                            key={Config('app.twitter_handle')}
+                                            className="flex items-center font-medium whitespace-nowrap px-5 mt-6"
+                                        >
+                                            <img
+                                                src={post.author?.picture}
+                                                alt=""
+                                                className="mr-3 w-9 h-9 rounded-full bg-slate-50 dark:bg-slate-800"
+                                                decoding="async"
+                                            />
+                                            <div className="text-sm leading-4">
+                                                <div className="text-slate-900 dark:text-slate-200">
+                                                    {post.author?.name}
+                                                </div>
+                                                <div className="mt-1">
+                                                    {Config('app.twitter_handle') && (
+                                                        <a
+                                                            href={`https://twitter.com/${Config('app.twitter_handle').replace('@', '')}`}
+                                                            className="text-primary-500 hover:text-primary-600 dark:text-primary-400"
+                                                        >
+                                                            {Config('app.twitter_handle')}
+                                                        </a>
+                                                    )}
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div className="mt-12 prose prose-slate dark:prose-dark">
+                                    <div dangerouslySetInnerHTML={{ __html: content }} />
+                                </div>
+                            </article>
+                        </main>
+                        <footer className="mt-16">
+                            {Config('app.convert_action_url') && (
+                                <div className="relative">
+                                    <section className="relative py-16 border-t border-slate-200 dark:border-slate-200/5">
+                                        <h2 className="text-xl font-semibold text-slate-900 tracking-tight dark:text-white">
+                                            Get all of our updates directly to your inbox.
+                                            <br />
+                                            Sign up for our newsletter.
+                                        </h2>
+                                        <div className="mt-5 max-w-md">
+                                            <NewsletterForm 
+                                                action={Config('app.convert_action_url')} 
+                                            />
+                                        </div>
+                                    </section>
+                                </div>
+                            )}
+                            <div className="relative">
+                                <section className="relative py-16 border-t border-slate-200 dark:border-slate-200/5">
+                                <div className='pt-8 pb-10 text-center text-slate-500 dark:border-slate-200/5'>
+                                    Built with <a href='https://elegantframework.com/' 
+                                        aria-label='Built with the Elegant framework'
+                                        className='font-semibold hover:text-primary-500 dark:hover:text-primary-400'
+                                    >
+                                        Elegant
+                                    </a>.
+                                </div>
+                                </section>
+                            </div>
+                        </footer>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const post = getDocumentBySlug('posts', context.params?.slug as string, [
+        'title',
+        'status',
+        'author',
+        'slug',
+        'description',
+        'coverImage',
+        'publishedAt',
+        'content',
+        'section'
+    ]);
+
+    const content = await MarkdownToHtml(post.content);
+    
+    return {
+        props: { 
+            post,
+            content,
+        }
+    };
+};
