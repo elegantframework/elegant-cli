0784acd47b6075db1b28576231ce2244cffcca56
Brandon Owens <brandono@vitamins2you.com>
2023-09-13 15:18:17 -0400

    Update index.ts

--- packages/elegant-ui/src/index.ts
+++ packages/elegant-ui/src/index.ts
@@ -1,14 +1,14 @@
 import BuiltWithElegant from "./components/Marks/BuiltWithElegant/BuiltWithElegant";
 import Card from "./components/Cards/Card";
-import PoweredByElegant from "./components/Marks/PoweredByElegant/PoweredByElegant";
-import YouTubeIcon from "./components/Icons/YouTube/YouTubeIcon";
+import GitHubIcon from "./components/Icons/GitHub/GitHubIcon";
+import InstagramIcon from "./components/Icons/Instagram/InstagramIcon";
+import LinkedInIcon from "./components/Icons/LinkedIn/LinkedInIcon";
+import MoonIcon from "./components/Icons/Moon/MoonIcon";
 import PCIcon from "./components/Icons/PC/PCIcon";
+import PoweredByElegant from "./components/Marks/PoweredByElegant/PoweredByElegant";
 import SunIcon from "./components/Icons/Sun/SunIcon";
 import TwitterIcon from "./components/Icons/Twitter/TwitterIcon";
-import MoonIcon from "./components/Icons/Moon/MoonIcon";
-import LinkedInIcon from "./components/Icons/LinkedIn/LinkedInIcon";
-import InstagramIcon from "./components/Icons/Instagram/InstagramIcon";
-import GitHubIcon from "./components/Icons/GitHub/GitHubIcon";
+import YouTubeIcon from "./components/Icons/YouTube/YouTubeIcon";
 
 export {
     BuiltWithElegant,
