3e71de096ded35e2bde82e67cf14e8c843a63925
Brandon Owens <brandono@vitamins2you.com>
2023-10-01 12:56:30 -0400

    Link fix

--- source/elegant/src/components/core/Admin/Pages/login.tsx
+++ source/elegant/src/components/core/Admin/Pages/login.tsx
@@ -4,6 +4,7 @@ import Logo from "@/components/core/Logos/Logo/Logo";
 import Config from "@/utils/core/Config/Config";
 import MetaTitle from "@/utils/core/Meta/MetaTitle";
 import { ArrowLongLeftIcon } from "@heroicons/react/20/solid";
+import Link from "next/link";
 
 Login.layoutProps = {
   meta: {
@@ -22,19 +23,20 @@ export default function Login() {
         <p className="mb-5 dark:text-slate-200">
           Sign in with GitHub to access your dashboard.
         </p>
-        <a
-          href="/api/admin/login"
+        <Link
           className="mr-2 mb-2 inline-flex items-center rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white bg-primary-600 hover:bg-primary-600 dark:hover:bg-primary-400 focus:outline-none focus:ring-2 focus:ring-primary-500 focus:ring-offset-2 focus:ring-primary-300 dark:focus:ring-offset-primary-900 dark:focus:ring-primary-700"
+          href={"/api/admin/login"}
         >
           <GitHubIcon className="h-5 w-5 mr-2"/> Sign in with GitHub
-        </a>
+        </Link>
       </Card>
       <div className="sm:mx-auto sm:w-full sm:max-w-md xs:pl-0 sm:pl-4 text-left -mt-6 dark:text-white">
-        <a href="/"
-           className="hover:underline"
+        <Link 
+          href="/"
+          className="hover:underline"
         >
           <ArrowLongLeftIcon className="h-6 w-6 inline mr-2"/>Go to {Config("app.name")}
-        </a>
+        </Link>
       </div>
     </main>
   )
