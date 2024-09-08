12a54ee3470bf3c7d167197715197f13f019a0ce
Brandon Owens <brandono@vitamins2you.com>
2024-09-07 23:19:58 -0400

    r2 fixes

--- packages/elegant/src/utils/CloudFlare/R2.ts
+++ packages/elegant/src/utils/CloudFlare/R2.ts
@@ -13,7 +13,6 @@ export default async function SaveFile(file: File) {
     await axios.put(preSignedUrl || "", file, {
       headers: {
         "Content-Type": file.type,
-        "Access-Control-Allow-Origin": "*"
       }
     });
 
